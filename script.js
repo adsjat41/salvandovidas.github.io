b36a0ea11aa4ea5f92083a427e1c6493bc9bdfaa
adsjat41 <adsjat62@gmail.com>
2024-12-13 13:37:56 -0600

    innitial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
+});
